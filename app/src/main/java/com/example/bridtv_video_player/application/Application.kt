598de76390690a7fb6f02a58fd4e4490adc9c058
Luka Jeremic <ljeremic4019rn@raf.rs>
2023-11-10 15:51:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/bridtv_video_player/application/Application.kt
@@ -0,0 +1,14 @@
+package com.example.bridtv_video_player.application
+
+import android.app.Application
+
+
+class Application : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+//        init()
+    }
+
+
+}
